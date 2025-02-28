a67fa21259eafcf12eaeba5fa2c36b8f12283b89
ib-fello <ibrahimafello2017@gmail.com>
2025-02-28 16:47:59 +0100

    feat: bug serveur

--- rest-api/src/main/java/m1_miage/tlse/ioe/G5/moovly/endpoints/UserEndpoint.java
+++ rest-api/src/main/java/m1_miage/tlse/ioe/G5/moovly/endpoints/UserEndpoint.java
@@ -1,11 +1,11 @@
 package m1_miage.tlse.ioe.G5.moovly.endpoints;
 
 
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import m1_miage.tlse.ioe.G5.moovly.errors.*;
+import m1_miage.tlse.ioe.G5.moovly.errors.BadRequestErrorResponse;
+import m1_miage.tlse.ioe.G5.moovly.errors.CreationFailedErrorResponse;
 import m1_miage.tlse.ioe.G5.moovly.request.UserCreationRequest;
 import m1_miage.tlse.ioe.G5.moovly.response.UserResponseDTO;
 import org.springframework.http.HttpStatus;
@@ -20,13 +20,14 @@ import java.util.List;
 public interface UserEndpoint {
 
     @ApiResponse(responseCode = "201", description = "Création réussie")
-    @ApiResponse(responseCode = "500", description = "Problème de création")
     @ApiResponse(responseCode = "400" ,
             description = "L'utilisateur n'a pas été créer",
-            content = @Content(schema = @Schema(implementation = String.class),
+            content = @Content(schema = @Schema(implementation = BadRequestErrorResponse.class),
                     mediaType = MediaType.
             APPLICATION_JSON_VALUE))
-    @ResponseStatus(HttpStatus.CREATED)
+    @ApiResponse(   responseCode = "500",
+            description = "Echec de la création d'un utilisateur",
+            content = @Content(schema = @Schema(implementation = CreationFailedErrorResponse.class)))    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/create")
     UserResponseDTO createUser(@RequestBody UserCreationRequest userCreationRequest);
 
@@ -48,19 +49,7 @@ public interface UserEndpoint {
     @DeleteMapping("/{email}")
     void deleteUserByEmail(@PathVariable(name = "email") String email);
 
-    @Operation(description = "Renvoie le nombre d’incidents signalés par un utilisateur")
-    @ApiResponse(   responseCode = "200", description = "Nombre de signalements renvoyé avec succès")
-    @ApiResponse(   responseCode = "400",
-                    description = "Requête invalide",
-                    content = @Content(schema = @Schema(implementation = BadRequestErrorResponse.class)))
-    @ApiResponse(   responseCode = "404",
-                    description  = "Utilisateur non trouvé",
-                    content = @Content(schema = @Schema(implementation = UserNotFoundErrorResponse.class), mediaType = MediaType.APPLICATION_JSON_VALUE))
-    @ApiResponse(   responseCode = "500",
-                    description = "Echec de la recupération du nombre d'incidents",
-                    content = @Content(schema = @Schema(implementation = GettingFailedErrorResponse.class)))
-    @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/{email}/reports/total")
-    int getNumberOfReportsByEmailUser(@PathVariable(name = "email") String email) ;
+
+
 }
 
